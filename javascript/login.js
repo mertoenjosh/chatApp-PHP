cf20a6f7684ae80b3db4ad9b2dfa7d800af9ad8f
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-16 14:15:35 +0300

    create the login

---
+++ javascript/login.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const form = document.querySelector('.login form');
+let continueBtn = form.querySelector('.button input');
+let errorText = form.querySelector('.error-txt');
+
+form.onsubmit = e => {
+  e.preventDefault();
+};
+
+continueBtn.onclick = () => {
+  //let's start ajax
+  let xhr = new XMLHttpRequest(); // creating XML object
+  xhr.open('POST', 'php/signup.php', true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+
+        if (data === 'Success') {
+          location.href = 'users.php';
+        } else {
+          errorText.textContent = data;
+          errorText.style.display = 'block';
+        }
+      }
+    }
+  };
+
+  //   send the form data through ajax to php
+  let formData = new FormData(form); // create a new formData object
+
+  xhr.send(formData);
+};
