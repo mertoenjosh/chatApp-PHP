3303a27f709db063010239e014981a4f74a4d768
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-19 00:59:55 +0300

    commit

--- javascript/users.js
+++ javascript/users.js
@@ -8,6 +8,34 @@ searchBtn.onclick = () => {
   searchBar.classList.toggle('active');
   searchBar.focus();
   searchBtn.classList.toggle('active');
+  searchBar.value = '';
+};
+
+searchBar.onkeyup = () => {
+  let searchTerm = searchBar.value;
+
+  if (searchTerm != '') {
+    searchBar.classList.add('active');
+  } else {
+    searchBar.classList.remove('active');
+  }
+
+  // let's start AJAX
+
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('POST', 'php/search.php', true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        userList.innerHTML = data;
+      }
+    }
+  };
+
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.send('searchTerm=' + searchTerm);
 };
 
 setInterval(() => {
@@ -20,7 +48,10 @@ setInterval(() => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         let data = xhr.response;
-        userList.innerHTML = data;
+
+        if (!searchBar.classList.contains('active')) {
+          userList.innerHTML = data;
+        }
       }
     }
   };
