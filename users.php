8d06ad0be8d5b4c8fcc4c3f776bd7984bf1aac77
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-09-18 23:39:17 +0300

    made current user data dynamic

--- users.php
+++ users.php
@@ -1,30 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Realtime Chat App | Users</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    />
-  </head>
+<?php
+  session_start();
+
+  // redirect user to login if session is not set
+  if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+  }
+
+?>
+
+<?php include_once "header.php" ?>
   <body>
     <div class="wrapper">
       <section class="users">
         <header>
 
         <?php
+          include_once "php/config.php";
 
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+
+          if (mysqli_num_rows($sql) > 0) {
+            $row = mysqli_fetch_assoc($sql);
+          }
         ?>
 
           <div class="content">
-            <img src="logo2.png" alt="" />
+            <img src="php/images/<?php echo $row['img'] ?>" alt="" />
             <div class="details">
-              <span>Martin Thuo</span>
-              <p>Active now</p>
+              <span><?php echo $row['fname'] . " " . $row['lname']?> </span>
+              <p><?php echo $row['u_status'] ?></p>
             </div>
           </div>
           <a href="#" class="logout">Logout</a>
@@ -110,7 +114,7 @@
         </div>
       </section>
     </div>
-    
+
     <script src="javascript/users.js" defer></script>
 
   </body>
