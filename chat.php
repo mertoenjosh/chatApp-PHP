9964119d9fa3bc3b2f459786ebbcaae90a7ea009
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-10-03 12:35:16 +0300

    make selected chat dynamic

--- chat.php
+++ chat.php
@@ -1,27 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Realtime Chat App | Chat area</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    />
-  </head>
+<?php
+  session_start();
+  if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+  }
+
+?>
+
+<?php include_once "header.php" ?>
+
   <body>
     <div class="wrapper">
       <section class="chat-area">
         <header>
-          <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-          <img src="logo2.png" alt="" />
+        <?php
+          include_once "php/config.php";
+          $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }
+
+        ?>
+
+          <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+          <img src="php/images/<?php echo $row['img'] ?>" alt="" />
           <div class="details">
-            <span>Martin Thuo</span>
-            <p>Active now</p>
+            <span><?php echo $row['fname']. " ". $row['lname'] ?></span>
+            <p><?php echo $row['u_status'] ?></p>
           </div>
         </header>
+
         <div class="chat-box">
           <div class="chat outgoing">
             <div class="details">
