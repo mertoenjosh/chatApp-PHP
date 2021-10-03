cfee560115e817f57961646dde448f0527cd40ee
Martin N Thuo <mertoenjosh3527@gmail.com>
2021-10-03 17:23:10 +0300

    done with messages

--- php/data.php
+++ php/data.php
@@ -2,19 +2,35 @@
 
 while ($row = mysqli_fetch_assoc($sql)) {
   $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
-            OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$row['unique_id']}
-            OR outgoing_msg_id = {$row['unique_id']})";
+            OR outgoing_msg_id = '{$row['unique_id']}') AND (outgoing_msg_id = '{$outgoing_id}'
+            OR incoming_msg_id = '{$outgoing_id}') ORDER BY msg_id DESC LIMIT 1";
 
-    $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
-                  <div class="content">
-                    <img src="php/images/' . $row['img'] . ' " alt="" />
-                    <div class="details">
-                      <span>' . $row['fname'] . " " . $row['lname'] . '</span>
-                      <p>This is a test message</p>
-                    </div>
+  $query2 = mysqli_query($conn, $sql2);
+  $row2 = mysqli_fetch_assoc($query2);
+
+  if(mysqli_num_rows($query2) > 0) {
+    $result = $row2['msg'];
+  } else {
+    $result = "No message available";
+  }
+
+  // trim message if a words are more than 28 
+  (strlen($result) > 28) ? $msg = substr($result, 0, 28).'...' : $msg = $result;
+
+  // adding you: text before msg if login send msg
+  ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "you: " : $you = "";
+  
+
+  $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
+                <div class="content">
+                  <img src="php/images/' . $row['img'] . ' " alt="" />
+                  <div class="details">
+                    <span>' . $row['fname'] . " " . $row['lname'] . '</span>
+                    <p>'. $you . $msg .'</p>
                   </div>
-                  <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>';
-}
+                </div>
+                <div class="status-dot"><i class="fas fa-circle"></i></div>
+              </a>';
+            };
 
 ?>
